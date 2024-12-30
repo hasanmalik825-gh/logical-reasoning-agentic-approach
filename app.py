4cc4d788d23e8d2a2ae2ee09611a5fcff4548345
hasanmalik825-gh <gclab825@gmail.com>
2024-12-30 21:17:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ st.write("Ask any logical reasoning question.")
 
 user_input=st.chat_input(placeholder="Enter your question here...")
 
-if st.button("Reason now."):
-    if user_input and groq_api_key:
-        agent = get_reasoning_agent(groq_api_key)
-        st.write(user_input)
-        st.info("Thinking...")
-        response = agent.invoke({"input": user_input})
-        st.success("Done!")
-        with st.expander("Agentic Response.", expanded=True):
-            st.write(response["output"])
-    else:
-        st.warning("Please enter both, your Groq API key and question.")
+if user_input and groq_api_key:
+    agent = get_reasoning_agent(groq_api_key)
+    st.write(user_input)
+    st.info("Thinking...")
+    response = agent.invoke({"input": user_input})
+    st.success("Done!")
+    with st.expander("Agentic Response.", expanded=True):
+        st.write(response["output"])
+else:
+    st.warning("Please enter both, your Groq API key and question.")
